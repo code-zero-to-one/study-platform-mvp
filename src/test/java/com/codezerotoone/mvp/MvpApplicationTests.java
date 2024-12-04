b741c44c943865101fdf566190ebe08ef9859305
SEONG JIN JO <123255935+seong-jin-jo@users.noreply.github.com>
2024-12-05 00:28:47 +0900

    init

---
+++ src/test/java/com/codezerotoone/mvp/MvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codezerotoone.mvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
